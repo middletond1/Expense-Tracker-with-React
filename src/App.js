ecfbf2437985c95e3c562e490fa4ee6e6f621d19
David Middleton <middleton.davida@gmail.com>
2022-02-07 21:04:47 -0500

    formData state default set. handleFormChange function created, both formData and handleFormChange passed as props to Form component.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import Form from './form';
 import ExpenseTable from './expensetable';
 
 export default function App() {
-    
+    const [formData, setFormData] = React.useState(
+        {expense: "", amount: "", date: "", store: ""}
+    );
+
+    function handleFormChange(event) {
+        setFormData(prevFormData => {
+            const { name, value } = event.target
+            return {
+                ...prevFormData,
+                [name]: value
+            }
+        })
+    }
+
+    console.log(formData)
 
     return (
         <div>
-            <Form />
+            <Form 
+                handleFormChange={handleFormChange}
+                formData={formData}
+            />
             <ExpenseTable />
         </div>
     )
--- src/form.js
+++ src/form.js
@@ -1,9 +1,45 @@
 import React from 'react'
 
-export default function Form() {
+export default function Form(props) {
     return (
         <div>
             <form>
+                <label htmlFor="expense">Expense:</label>
+                <input 
+                    type="text" 
+                    name="expense" 
+                    placeholder="Enter Item"
+                    value={props.formData.expense}
+                    onChange={props.handleFormChange}
+                    required 
+                />
+                <label htmlFor="amount">Amount:</label>
+                <input 
+                    type="number" 
+                    name="amount" 
+                    step=".01" 
+                    placeholder="Price" 
+                    value={props.formData.amount}
+                    onChange={props.handleFormChange}
+                    required 
+                />
+                <label htmlFor="date">Date:</label>
+                <input 
+                    type="date" 
+                    name="date"
+                    value={props.formData.date}
+                    onChange={props.handleFormChange}
+                    required 
+                />
+                <label htmlFor="store">Store:</label>
+                <input 
+                    type="text" 
+                    name="store" 
+                    placeholder="Where was it bought?" 
+                    value={props.formData.store}
+                    onChange={props.handleFormChange}
+                />
+                <button type="submit">Add Expense</button>
             </form>
         </div>
     )
