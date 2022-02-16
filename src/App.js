891eec1a388797fb34763aa7c4e001ee8e1a8b3a
David Middleton <middleton.davida@gmail.com>
2022-02-15 20:47:35 -0500

    added date functions to expensetable.js to make date easier to read

--- src/App.js
+++ src/App.js
@@ -57,7 +57,6 @@ export default function App() {
 
     React.useEffect(() => localStorage.setItem('tabledata', JSON.stringify(tableData)), [tableData])
 
-    console.log(formData)
     console.log(tableData)
     
     return (
--- src/expensetable.js
+++ src/expensetable.js
@@ -2,11 +2,44 @@ import React from 'react'
 import Table from 'react-bootstrap/Table'
 
 export default function ExpenseTable(props) {
+    
+    function parseDay(date) {
+        const day = date.substr(8, 2);
+        return day;
+    }
+    
+    function parseMonth(date) {
+        const month = parseInt(date.substr(5, 2)) - 1;
+        return month;
+    }
+    
+    function monthToCalendar(month) {
+        const months = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+        ]
+        return months[month];
+    }
+    
+    function parseYear(date) {
+        const year = date.substr(0, 4);
+        return year;
+    }
     const tableElements = props.tableData.map(item => (
             <tr key={item.id}>
                 <td>{item.expense}</td>
                 <td>$ {item.amount}</td>
-                <td>{item.date}</td>
+                <td>{`${monthToCalendar(parseMonth(item.date))} ${parseDay(item.date)}, ${parseYear(item.date)}`}</td>
                 <td>{item.store}</td>
                 <td><button id={item.id} onClick={props.removeTableData}>Delete</button></td>
             </tr>
